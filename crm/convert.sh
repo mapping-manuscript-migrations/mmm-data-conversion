527022d80da15d773613c514ec047d405cdec145
Mikko Koho <mikko.koho@aalto.fi>
2018-04-18 10:44:56 +0300

    Send the query differently to fix escaping issues

--- crm/convert.sh
+++ crm/convert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # run the SPARQL construct query
-echo 'query=' | cat - $FUSEKI_HOME/construct_cidoc_crm.sparql | curl -d @- $INPUT_SPARQL_ENDPOINT -v > /tmp/cidoc.ttl
+curl -f --data-urlencode "query=$(cat $FUSEKI_HOME/construct_cidoc_crm.sparql)" $INPUT_SPARQL_ENDPOINT -v > /tmp/cidoc.ttl
 
 # load the result to a new Fuseki
 $FUSEKI_HOME/tdbloader --graph=http://ldf.fi/mmm-sdbm-cidoc-crm/ /tmp/cidoc.ttl \
