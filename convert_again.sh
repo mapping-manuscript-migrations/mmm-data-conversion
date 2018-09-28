7c4cc5f87cbee0945d461352b79e484cb79ed41e
Mikko Koho <mikko.koho@aalto.fi>
2018-09-28 10:23:04 +0300

    Update convert_again.sh

--- convert_again.sh
+++ convert_again.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
-# TODO: Update
 docker-compose down \
   && docker volume rm mmmsdbmdata_mmm-crm \
   && docker-compose build crm \
-  && docker-compose up -d input \
+  && docker-compose up -d input-sdbm \
+  && docker-compose up -d input-bibale \
+  && docker-compose up -d input-bodley \
   && sleep 5 \
   && docker-compose run --rm crm ./convert.sh \
   && docker-compose up -d crm
