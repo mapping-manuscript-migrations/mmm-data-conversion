7caab2a894895771d73c30fc7e956949ceb596bf
Mikko Koho <mikko.koho@aalto.fi>
2018-11-28 15:07:11 +0200

    Convert Bodley people
    Co-authored-by: Esko Ikkala <esko.ikkala@aalto.fi>

--- crm/convert_bodley.sh
+++ crm/convert_bodley.sh
@@ -5,8 +5,10 @@ set -eo pipefail
 # run the SPARQL construct query
 echo 'Constructing Bodley manuscripts'
 curl -f --data-urlencode "query=$(cat $FUSEKI_HOME/construct_bod_manuscripts.sparql)" $INPUT_BODLEY_SPARQL_ENDPOINT -v > /tmp/bodley_manuscripts.ttl
+echo 'Constructing Bodley people'
+curl -f --data-urlencode "query=$(cat $FUSEKI_HOME/construct_bod_people.sparql)" $INPUT_BODLEY_SPARQL_ENDPOINT -v > /tmp/bodley_people.ttl
 
 echo "Inserting data to Fuseki"
-cat /tmp/bodley_manuscripts.ttl > /tmp/bodley_cidoc.ttl
+cat /tmp/bodley_manuscripts.ttl /tmp/bodley_people.ttl > /tmp/bodley_cidoc.ttl
 
 exec "$@"
