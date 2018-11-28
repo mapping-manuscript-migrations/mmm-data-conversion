80cd7cff7aef79e07a90063260cb05c19dccad27
Mikko Koho <mikko.koho@aalto.fi>
2018-11-28 15:36:18 +0200

    Convert Bodley places

--- crm/convert_bodley.sh
+++ crm/convert_bodley.sh
@@ -7,8 +7,10 @@ echo 'Constructing Bodley manuscripts'
 curl -f --data-urlencode "query=$(cat $FUSEKI_HOME/construct_bod_manuscripts.sparql)" $INPUT_BODLEY_SPARQL_ENDPOINT -v > /tmp/bodley_manuscripts.ttl
 echo 'Constructing Bodley people'
 curl -f --data-urlencode "query=$(cat $FUSEKI_HOME/construct_bod_people.sparql)" $INPUT_BODLEY_SPARQL_ENDPOINT -v > /tmp/bodley_people.ttl
+echo 'Constructing Bodley places'
+curl -f --data-urlencode "query=$(cat $FUSEKI_HOME/construct_bod_places.sparql)" $INPUT_BODLEY_SPARQL_ENDPOINT -v > /tmp/bodley_places.ttl
 
 echo "Inserting data to Fuseki"
-cat /tmp/bodley_manuscripts.ttl /tmp/bodley_people.ttl > /tmp/bodley_cidoc.ttl
+cat /tmp/bodley_manuscripts.ttl /tmp/bodley_people.ttl /tmp/bodley_places.ttl > /tmp/bodley_cidoc.ttl
 
 exec "$@"
