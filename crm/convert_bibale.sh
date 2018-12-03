b6c2c77152f30f516c328e72124956903d4c2d44
Mikko Koho <mikko.koho@aalto.fi>
2018-12-03 16:53:43 +0200

    Run all Bibale conversion

--- crm/convert_bibale.sh
+++ crm/convert_bibale.sh
@@ -5,8 +5,12 @@ set -eo pipefail
 # run the SPARQL construct query
 echo 'Constructing Bibale manuscripts'
 curl -f --data-urlencode "query=$(cat $FUSEKI_HOME/construct_bibale_manuscripts.sparql)" $INPUT_BIBALE_SPARQL_ENDPOINT -v > /tmp/bibale_manuscripts.ttl
+echo 'Constructing Bibale places'
+curl -f --data-urlencode "query=$(cat $FUSEKI_HOME/construct_bibale_places.sparql)" $INPUT_BIBALE_SPARQL_ENDPOINT -v > /tmp/bibale_places.ttl
+echo 'Constructing Bibale actors'
+curl -f --data-urlencode "query=$(cat $FUSEKI_HOME/construct_bibale_actors.sparql)" $INPUT_BIBALE_SPARQL_ENDPOINT -v > /tmp/bibale_actors.ttl
 
 echo "Inserting data to Fuseki"
-cat /tmp/bibale_manuscripts.ttl  > /tmp/bibale_cidoc.ttl
+cat /tmp/bibale_manuscripts.ttl /tmp/bibale_places.ttl /tmp/bibale_actors.ttl > /tmp/bibale_cidoc.ttl
 
 exec "$@"
