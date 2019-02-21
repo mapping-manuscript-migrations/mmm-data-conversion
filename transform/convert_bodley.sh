cb8ac743fcbb389116d3b8e7d3b0f0ed788e5c93
Mikko Koho <mikko.koho@aalto.fi>
2019-02-21 16:11:16 +0200

    Bodley: Add new SPARQL constructs to script

--- transform/convert_bodley.sh
+++ transform/convert_bodley.sh
@@ -5,12 +5,16 @@ set -eo pipefail
 # run the SPARQL construct query
 printf '\nConstructing Bodley manuscripts\n\n'
 curl -f --data-urlencode "query=$(cat /app/construct_bod_manuscripts.sparql)" $INPUT_BODLEY_SPARQL_ENDPOINT -v > $OUTPUT/_bodley_manuscripts.ttl
+printf '\nConstructing Bodley works\n\n'
+curl -f --data-urlencode "query=$(cat /app/construct_bod_works.sparql)" $INPUT_BODLEY_SPARQL_ENDPOINT -v > $OUTPUT/_bodley_works.ttl
+printf '\nConstructing Bodley expressions\n\n'
+curl -f --data-urlencode "query=$(cat /app/construct_bod_expressions.sparql)" $INPUT_BODLEY_SPARQL_ENDPOINT -v > $OUTPUT/_bodley_expressions.ttl
 printf '\nConstructing Bodley people\n\n'
 curl -f --data-urlencode "query=$(cat /app/construct_bod_people.sparql)" $INPUT_BODLEY_SPARQL_ENDPOINT -v > $OUTPUT/_bodley_people.ttl
 printf '\nConstructing Bodley places\n\n'
 curl -f --data-urlencode "query=$(cat /app/construct_bod_places.sparql)" $INPUT_BODLEY_SPARQL_ENDPOINT -v > $OUTPUT/_bodley_places.ttl
 
-cat $OUTPUT/_bodley_manuscripts.ttl $OUTPUT/_bodley_people.ttl $OUTPUT/_bodley_places.ttl > $OUTPUT/_bodley_combined.ttl
+cat $OUTPUT/_bodley_manuscripts.ttl $OUTPUT/_bodley_works.ttl $OUTPUT/_bodley_expressions.ttl $OUTPUT/_bodley_people.ttl $OUTPUT/_bodley_places.ttl > $OUTPUT/_bodley_combined.ttl
 
 printf '\nLinking Bodley places\n\n'
 
