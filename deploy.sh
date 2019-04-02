0ba6de6aeaf8da9ae83faa82d4f6046a08fbf711
Mikko Koho <mikko.koho@iki.fi>
2019-04-02 18:43:11 +0300

    Really do remove the crm volume in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@ set -eo pipefail
 
 docker-compose build crm
 
-docker-compose rm -vsf crm  # Force stop and remove crm volume
+docker-compose rm -vsf crm
+docker volume rm mmm-data-conversion_mmm-crm
+
 docker-compose run --rm crm ./deploy.sh
 docker-compose up -d crm
