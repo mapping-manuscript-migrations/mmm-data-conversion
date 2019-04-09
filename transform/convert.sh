80e7d508799ae0811b97abba0407ee42eb301841
Mikko Koho <mikko.koho@aalto.fi>
2019-04-09 19:26:52 +0300

    Do all manuscript linking from convert.sh

--- transform/convert.sh
+++ transform/convert.sh
@@ -20,10 +20,7 @@ case "$1" in
   ;;
 esac
 
-printf '\nAdding manual manuscript links\n\n'
-python manuscripts.py manual_links /output/bibale_cidoc.ttl /output/bodley_cidoc.ttl /output/sdbm_cidoc.ttl --input_csv /data/manuscript_links.csv --logfile /output/logs/manuscript_linking.log
-
-printf '\nLinking with Phillipps numbers\n\n'
-python manuscripts.py link_phillipps /output/bibale_cidoc.ttl /output/bodley_cidoc.ttl /output/sdbm_cidoc.ttl --logfile /output/logs/manuscript_linking.log
+printf '\nAdding manual manuscript links and shelfmark links\n\n'
+python manuscripts.py all /output/bibale_cidoc.ttl /output/bodley_cidoc.ttl /output/sdbm_cidoc.ttl --input_csv /data/manuscript_links.csv --logfile /output/logs/manuscript_linking.log
 
 chmod a+r $OUTPUT/*
