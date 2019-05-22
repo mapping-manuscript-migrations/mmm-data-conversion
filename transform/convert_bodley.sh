4e791f02679f7450b87d2130417b8fbb27554c1a
Mikko Koho <mikko.koho@aalto.fi>
2019-05-22 14:30:25 +0300

    Transform and harmonize languages

--- transform/convert_bodley.sh
+++ transform/convert_bodley.sh
@@ -21,8 +21,10 @@ printf '\nConstructing Bodley places\n\n'
 curl -f --data-urlencode "query=$(cat /app/construct_bod_places.sparql)" $INPUT_BODLEY_SPARQL_ENDPOINT -v > /output/_bodley_places.ttl
 printf '\nConstructing Bodley observations\n\n'
 curl -f --data-urlencode "query=$(cat /app/construct_bod_observations.sparql)" $INPUT_BODLEY_SPARQL_ENDPOINT -v > /output/_bodley_observations.ttl
+printf '\nConstructing Bodley languages\n\n'
+curl -f --data-urlencode "query=$(cat /app/construct_bod_languages.sparql)" $INPUT_BODLEY_SPARQL_ENDPOINT -v > /output/_bodley_languages.ttl
 
-cat /output/_bodley_phillipps.ttl /output/_bodley_manuscripts.ttl /output/_bodley_works.ttl /output/_bodley_expressions.ttl /output/_bodley_people.ttl /output/_bodley_places.ttl /output/_bodley_observations.ttl > /output/_bodley_combined.ttl
+cat /output/_bodley_phillipps.ttl /output/_bodley_manuscripts.ttl /output/_bodley_works.ttl /output/_bodley_expressions.ttl /output/_bodley_people.ttl /output/_bodley_places.ttl /output/_bodley_observations.ttl /output/_bodley_languages.ttl > /output/_bodley_combined.ttl
 
 printf '\nLinking Bodley places\n\n'
 
--- transform/convert_sdbm.sh
+++ transform/convert_sdbm.sh
@@ -2,31 +2,33 @@
 
 set -eo pipefail
 
-rm -f $OUTPUT/_sdbm*
-rm -f $OUTPUT/mmm_sdbm.ttl
+rm -f /output/_sdbm*
+rm -f /output/mmm_sdbm.ttl
 
 # run the SPARQL construct query
 
 printf '\nConstructing SDBM manuscripts transactions\n\n'
-curl -f --data-urlencode "query=$(cat /app/construct_sdbm_manuscripts_transactions.sparql)" $INPUT_SDBM_SPARQL_ENDPOINT -v > $OUTPUT/_sdbm_manuscripts_transactions.ttl
+curl -f --data-urlencode "query=$(cat /app/construct_sdbm_manuscripts_transactions.sparql)" $INPUT_SDBM_SPARQL_ENDPOINT -v > /output/_sdbm_manuscripts_transactions.ttl
 printf '\nConstructing SDBM manuscripts\n\n'
-curl -f --data-urlencode "query=$(cat /app/construct_sdbm_manuscripts.sparql)" $INPUT_SDBM_SPARQL_ENDPOINT -v > $OUTPUT/_sdbm_manuscripts.ttl
+curl -f --data-urlencode "query=$(cat /app/construct_sdbm_manuscripts.sparql)" $INPUT_SDBM_SPARQL_ENDPOINT -v > /output/_sdbm_manuscripts.ttl
 printf '\nConstructing SDBM works\n\n'
-curl -f --data-urlencode "query=$(cat /app/construct_sdbm_works.sparql)" $INPUT_SDBM_SPARQL_ENDPOINT -v > $OUTPUT/_sdbm_works.ttl
+curl -f --data-urlencode "query=$(cat /app/construct_sdbm_works.sparql)" $INPUT_SDBM_SPARQL_ENDPOINT -v > /output/_sdbm_works.ttl
 printf '\nConstructing SDBM actors\n\n'
-curl -f --data-urlencode "query=$(cat /app/construct_sdbm_people.sparql)" $INPUT_SDBM_SPARQL_ENDPOINT -v > $OUTPUT/_sdbm_people.ttl
+curl -f --data-urlencode "query=$(cat /app/construct_sdbm_people.sparql)" $INPUT_SDBM_SPARQL_ENDPOINT -v > /output/_sdbm_people.ttl
 printf '\nConstructing SDBM places\n\n'
-curl -f --data-urlencode "query=$(cat /app/construct_sdbm_places.sparql)" $INPUT_SDBM_SPARQL_ENDPOINT -v > $OUTPUT/_sdbm_places.ttl
+curl -f --data-urlencode "query=$(cat /app/construct_sdbm_places.sparql)" $INPUT_SDBM_SPARQL_ENDPOINT -v > /output/_sdbm_places.ttl
 printf '\nConstructing SDBM sources\n\n'
-curl -f --data-urlencode "query=$(cat /app/construct_sdbm_sources.sparql)" $INPUT_SDBM_SPARQL_ENDPOINT -v > $OUTPUT/_sdbm_sources.ttl
+curl -f --data-urlencode "query=$(cat /app/construct_sdbm_sources.sparql)" $INPUT_SDBM_SPARQL_ENDPOINT -v > /output/_sdbm_sources.ttl
+printf '\nConstructing SDBM languages\n\n'
+curl -f --data-urlencode "query=$(cat /app/construct_sdbm_languages.sparql)" $INPUT_SDBM_SPARQL_ENDPOINT -v > /output/_sdbm_languages.ttl
 
 # Take only relevant parts of the data for place linking, and combine with the rest later
 
-cat $OUTPUT/_sdbm_manuscripts_transactions.ttl $OUTPUT/_sdbm_manuscripts.ttl $OUTPUT/_sdbm_people.ttl $OUTPUT/_sdbm_places.ttl $OUTPUT/_sdbm_sources.ttl > $OUTPUT/_sdbm_combined.ttl
+cat /output/_sdbm_manuscripts_transactions.ttl /output/_sdbm_manuscripts.ttl /output/_sdbm_people.ttl /output/_sdbm_places.ttl /output/_sdbm_sources.ttl /output/_sdbm_languages.ttl > /output/_sdbm_combined.ttl
 
 printf '\nLinking SDBM places\n\n'
-python linker.py sdbm_places $OUTPUT/_sdbm_combined.ttl $OUTPUT/_sdbm_linked_places.ttl --logfile $OUTPUT/logs/sdbm_linking.log
+python linker.py sdbm_places /output/_sdbm_combined.ttl /output/_sdbm_linked_places.ttl --logfile /output/logs/sdbm_linking.log
 
-cat $OUTPUT/_sdbm_works.ttl $OUTPUT/_sdbm_linked_places.ttl > $OUTPUT/_sdbm_linked.ttl
+cat /output/_sdbm_works.ttl /output/_sdbm_linked_places.ttl > /output/_sdbm_linked.ttl
 
 exec "$@"
